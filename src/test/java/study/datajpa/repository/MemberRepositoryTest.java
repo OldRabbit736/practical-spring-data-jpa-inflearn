8f2ea783601a8f35e54eed854b4bc3e82653c979
OldRabbit736 <sylvan0212@gmail.com>
2022-07-01 18:52:45 +0900

    완료: 공통 인터페이스 설정

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -20,6 +20,8 @@ class MemberRepositoryTest {
 
     @Test
     public void testMember() {
+        System.out.println("memberRepository = " + memberRepository.getClass());
+
         Member member = new Member("memberA");
         Member savedMember = memberRepository.save(member);
 
