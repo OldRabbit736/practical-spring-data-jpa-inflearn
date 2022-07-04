dd63d2df9f8a0f027c9f94c46164b794b4d7100f
OldRabbit736 <sylvan0212@gmail.com>
2022-07-04 11:09:04 +0900

    완료: @Query, 리포지토리 메소드에 쿼리 정의하기

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -9,10 +9,23 @@ import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    // 메소드 이름으로 쿼리 생성
+    // 간단한 쿼리를 생성하는 경우라면 간간히 쓰인다.
+    // 그러나 복잡한 쿼리를 만드려 하면, 이름이 매우 길어지기 때문에 그 때는 @Query 방식을 사용한다.
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 
+    // 메소드 이름으로 쿼리 생성
     List<Member> findTop3By();
 
-    //@Query(name = "Member.findByUsername")
+    // 실무에서 NamedQuery는 거의 사용되지 않는다.
+    // 엔티티에 query문이 기록되어 있다.
+    @Query(name = "Member.findByUsername")
     List<Member> findByUsername(@Param("username") String username);
+
+    // 실무에서 많이 쓰는 방법!
+    // 메소드 이름에 제약이 없다. (즉 메소드 이름으로 쿼리 생성하는 경우처럼 이름을 복잡하게 짓지 않아도 된다.)
+    // 애플리케이션이 로드 되는 시점에 쿼리의 유효성을 체크해 주기 때문에 오류를 찾아내기 용이하다는 장점이 있다!
+    // 이름 없는 NamedQuery라고 할 수 있다.
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 }
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -93,4 +93,16 @@ class MemberRepositoryTest {
         assertEquals(1, members.size());
     }
 
+    @Test
+    void testQuery() {
+        Member member1 = new Member("AAA", 10);
+        Member member2 = new Member("BBB", 20);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        List<Member> members = memberRepository.findUser("AAA", 10);
+        assertSame(member1, members.get(0));
+        assertEquals(1, members.size());
+    }
+
 }
